cef4ebe217e975386ac85ffc464ef359e4ceafb0
MichaelArbel <michael.n.arbel@gmail.com>
2025-03-19 08:05:43 +0100

    update interface

---
+++ scripts/quadratic_toy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+parent_log_dir="data/outputs/debug"
+
+HYDRA_FULL_ERROR=1   OC_CAUSE=1 python -m ipdb main.py \
+		selection='BGS'\
+		training='quadratic_toy'\
+        +mlxp.logger.parent_log_dir=$parent_log_dir\
+
+
